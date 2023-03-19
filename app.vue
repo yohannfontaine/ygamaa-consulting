5600d37ed0788bb6292f5e452fdf2a9a5f6f8606
Yohann Fontaine <yohann.fontaine@gmail.com>
2023-03-19 19:54:10 +0100

    refactoring partner section

--- app.vue
+++ app.vue
@@ -17,32 +17,12 @@ const title = computed(() =>
     <HeaderSection />
     <!-- Hero section -->
     <Hero />
-
     <!-- About section -->
     <HeroBranding />
-
     <!-- Service section -->
     <ServiceIntro />
-
     <!-- Partners section -->
-    <div class="brand-logo-area section" id="partner">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="section-title-wrapper text-center section-space--mb_60">
-              <h2 class="section-title mb-15 wow move-up">
-                {{ $t("home.partners.title") }}
-              </h2>
-              <span class="section-text wow move-up">{{
-                $t("home.partners.description")
-              }}</span>
-            </div>
-          </div>
-        </div>
-
-        <BrandLogo />
-      </div>
-    </div>
+    <PartnerSection />
     <!-- Footer section -->
     <FooterSection />
   </NuxtLayout>
---
+++ components/PartnerSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="brand-logo-area section" id="partner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-title-wrapper text-center section-space--mb_60">
+            <h2 class="section-title mb-15 wow move-up">
+              {{ $t("home.partners.title") }}
+            </h2>
+            <span class="section-text wow move-up">{{
+              $t("home.partners.description")
+            }}</span>
+          </div>
+        </div>
+      </div>
+
+      <BrandLogo />
+    </div>
+  </div>
+</template>
